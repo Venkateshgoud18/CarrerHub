2b233e30c929c7c6d7554a101c3b46dce6469f39
Venkatesh goud <goudvenki5@gmail.com>
2026-02-28 09:07:11 -0800

    mongodb atlas commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
 const app=express();
 app.use(cors());
 app.use(express.json());
-
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected ✅");
+    } catch (err) {
+        console.error("MongoDB Error ❌:", err.message);
+        process.exit(1);
+    }
+}
+connectDB();
 const PORT=process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
